634f6c09234062637e009bdca6d26faf3d5293d4
tomask145 <thomas.krchnak@gmail.com>
2020-06-03 14:21:54 +0200

    Working with collections - video 5

---
+++ WorkingWithCollections/WorkingWithCollections/WorkingWithCollections/IPainter.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace WorkingWithCollections
+{
+    public interface IPainter
+    {
+        bool IsAvailable { get; }
+        TimeSpan EstimateTimeToPaint(double sqMeters);
+        double EstimateCompensation(double sqMeters);
+    }
+}
---
+++ WorkingWithCollections/WorkingWithCollections/WorkingWithCollections/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WorkingWithCollections
+{
+    class Program
+    {
+        private static IPainter FindCheapestPainter(double sqMeters, IEnumerable<IPainter> painters)
+        {
+            return painters
+                    .Where(p => p.IsAvailable)
+                    .WithMinimum(p => p.EstimateCompensation(sqMeters));
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
